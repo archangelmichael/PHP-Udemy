5b196f0b6fb9d38b5a02cf7d7fc8d509c1d7d29f
archangelmichael <radishikerov@yahoo.com>
2018-01-30 20:14:22 +0200

    Create initial php file.

---
+++ intro/intro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> PHP Udemy Intro</title>
+</head>
+<body>
+	<?php 
+	$pageTitle = "Hello World";
+	?>
+
+	<h1><?php echo $pageTitle; ?></h1>
+
+</body>
+</html>
