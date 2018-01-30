b56c146a373b91ee3f8b6623becd647b582edf1c
archangelmichael <radishikerov@yahoo.com>
2018-01-30 20:48:01 +0200

    PHP Variables and Arrays.

--- intro/intro.php
+++ intro/intro.php
@@ -1,15 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title> PHP Udemy Intro</title>
-</head>
-<body>
-	<?php 
-	$pageTitle = "Hello World";
-	?>
-
-	<h1><?php echo $pageTitle; ?></h1>
-
-</body>
+ <html lang="en">
+ 	<head>    
+   		<meta charset="UTF-8">
+    	<title> PHP Udemy Intro</title>
+   	</head> 
+    <body>
+     
+    	<h2>Hello PHP</h2>
+		<ul>
+			<li>
+				<p>Variables</p>
+				<?php 
+				$pageTitle = "Hello"; 
+				$firstName = "Archangel";
+				$lastName = "Michael";
+				echo $pageTitle . " " . $firstName . " " . $lastName . "<br>";
+				echo 5 + 5 . "<br>";
+				echo 5 * 6.5/7 . "<br>";
+				 ?>
+			</li>
+			<li>
+				<p>Regular arrays</p>
+				<?php
+				$numsArr = array(23,34,'asd','sdf','<span>Hi</span>');
+				echo $numsArr[2] . "<br>";
+				print_r($numsArr);
+				 ?>
+			</li>
+			<li>
+				<p>Associative Arrays</p>
+				<?php
+				$namesArr = array("first_name" => "ARCHANGEL");
+				echo $namesArr["first_name"] . "<br>";
+				print_r($namesArr);
+				 ?>
+			</li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</body> 
 </html>
---
+++ intro/variables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Udemy Variables</title>
+</head>
+<body>
+	
+	<?php 
+		$str = "Some string";
+		$num = 34;
+	?>
+
+</body>
+</html>
+
